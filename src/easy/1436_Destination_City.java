ecb2ca6856ceea99452c549ec15454c226d66946
arun.r <arunkumar.r@zohocorp.com>
2023-12-15 18:26:11 +0530

    added 1436_Destination_City

---
+++ src/easy/1436_Destination_City.java
@@ -0,0 +1,14 @@
+package easy;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Solution {
+
+    public String destCity(List<List<String>> paths) {
+
+        Map<String, String> map = paths.stream().collect(Collectors.toMap(p -> p.get(0), p -> p.get(1)));
+        return map.values().stream().filter(v -> !map.containsKey(v)).findFirst().get();
+    }
+}
