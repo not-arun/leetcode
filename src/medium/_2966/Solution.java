704f9c9b116f4bc14fd00e501b8f48fbdad3e95e
arun.r <arunkumar.r@zohocorp.com>
2024-02-01 12:46:34 +0530

    added problem 2966's solution

---
+++ src/medium/_2966/Solution.java
@@ -0,0 +1,19 @@
+package medium._2966;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int[][] divideArray(int[] nums, int k) {
+
+        Arrays.sort(nums);
+        int[][] res = new int[nums.length / 3][nums.length / 3];
+        for (int i = 0, j = 0; i < nums.length; j++, i += 3) {
+            if (nums[i + 2] - nums[i] > k) {
+                return new int[][]{};
+            }
+            res[j] = new int[]{nums[i], nums[i + 1], nums[i + 2]};
+        }
+        return res;
+    }
+}
